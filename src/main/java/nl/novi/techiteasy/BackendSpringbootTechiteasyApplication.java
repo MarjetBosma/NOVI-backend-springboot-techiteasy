60063f33784ecc2ff73b7c823bc333f1cdd27317
MarjetBosma <marjet_bosma@hotmail.com>
2023-11-12 15:03:18 +0100

    eerste commit

---
+++ src/main/java/nl/novi/techiteasy/BackendSpringbootTechiteasyApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringbootTechiteasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringbootTechiteasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/techiteasy/BackendSpringbootTechiteasyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootTechiteasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
